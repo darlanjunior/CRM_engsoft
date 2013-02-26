826b694489ea4611aecf4bde77eca25f9cbfbd93
darlan <darlan@darlan-pc.(none)>
2013-02-26 11:11:03 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRMEngsoft::Application.config.secret_token = '28282575d5de7df03bfad022dbc640968bc007d0f59a1356fece24aa8aeb963e4be1af95a423568b356c460e6414019ad312322a055808597f82d40ee8a4c8b9'
