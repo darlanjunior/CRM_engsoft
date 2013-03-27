fe6d44281a082e209e52a121788067d1e578ad4e
Felipe <felipe.castrosantana@gmail.com>
2013-03-27 04:11:24 -0300

    #15 Versao estavel do Acompanhamento de Chamados ( sem Eventos)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   attr_accessible :address, :clientId, :email, :name, :phone, :clientType
 
   has_many :support_cases
+  has_and_belongs_to_many :contact_groups
 end
