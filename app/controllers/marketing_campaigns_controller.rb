2f80b96ceb8c65b1c49a52ad355a35ed5f26b92e
alandbarroso <alandbarroso@gmail.com>
2013-04-02 16:40:49 -0300

    Outras evoluçoes no codigo das campanhas #5

--- app/controllers/marketing_campaigns_controller.rb
+++ app/controllers/marketing_campaigns_controller.rb
@@ -14,6 +14,8 @@ class MarketingCampaignsController < ApplicationController
   # GET /marketing_campaigns/1.json
   def show
     @marketing_campaign = MarketingCampaign.find(params[:id])
+    
+    @contact_groups = @marketing_campaign.contact_groups
 
     respond_to do |format|
       format.html # show.html.erb
@@ -25,6 +27,8 @@ class MarketingCampaignsController < ApplicationController
   # GET /marketing_campaigns/new.json
   def new
     @marketing_campaign = MarketingCampaign.new
+    
+    @contact_groups = ContactGroup.all
 
     respond_to do |format|
       format.html # new.html.erb
@@ -35,12 +39,22 @@ class MarketingCampaignsController < ApplicationController
   # GET /marketing_campaigns/1/edit
   def edit
     @marketing_campaign = MarketingCampaign.find(params[:id])
+    
+    @contact_groups = ContactGroup.all
   end
 
   # POST /marketing_campaigns
   # POST /marketing_campaigns.json
   def create
     @marketing_campaign = MarketingCampaign.new(params[:marketing_campaign])
+    
+    @marketing_campaign.contact_groups = Array.new
+    
+    if(params[:contact_groups] != nil)
+		  params[:contact_groups].each do |id|
+		  	@marketing_campaign.contact_groups << ContactGroup.find(id[0])
+			end
+		end
 
     respond_to do |format|
       if @marketing_campaign.save
@@ -57,6 +71,14 @@ class MarketingCampaignsController < ApplicationController
   # PUT /marketing_campaigns/1.json
   def update
     @marketing_campaign = MarketingCampaign.find(params[:id])
+    
+    @marketing_campaign.contact_groups = Array.new
+    
+    if(params[:contact_groups] != nil)
+		  params[:contact_groups].each do |id|
+		  	@marketing_campaign.contact_groups << ContactGroup.find(id[0])
+			end
+		end
 
     respond_to do |format|
       if @marketing_campaign.update_attributes(params[:marketing_campaign])
@@ -83,5 +105,19 @@ class MarketingCampaignsController < ApplicationController
   
   def delete
     @marketing_campaign = MarketingCampaign.find(params[:id])
+    
+    @contact_groups = @marketing_campaign.contact_groups
+  end
+  
+  def approve
+  	@marketing_campaign = MarketingCampaign.find(params[:id])
+  	
+  	@contact_groups = @marketing_campaign.contact_groups
+  end
+  
+  def desapprove
+  	@marketing_campaign = MarketingCampaign.find(params[:id])
+  	
+  	@contact_groups = @marketing_campaign.contact_groups
   end
 end
